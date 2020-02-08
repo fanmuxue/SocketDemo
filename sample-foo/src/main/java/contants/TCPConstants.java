78d865ebad2deb27b1ff2afde0ed497778280fcb
18229040618@163.com <18229040618@163.com>
2020-02-08 14:51:15 +0800

    工具类和依赖包

---
+++ sample-foo/src/main/java/contants/TCPConstants.java
@@ -0,0 +1,6 @@
+package contants;
+
+public class TCPConstants {
+    public static int PORT_SERVER = 30401;
+
+}
---
+++ sample-foo/src/main/java/contants/UDPConstants.java
@@ -0,0 +1,10 @@
+package contants;
+
+public class UDPConstants {
+    public static byte[] HEADER = new byte[]{7,7,7,7,7,7,7,7};
+    //服务器固话UDP接收端口
+    public static int PORT_SERVER=30201;
+    //客户端回送端口
+    public static int PORT_CLIENT_RESPONSE=30202;
+
+}
